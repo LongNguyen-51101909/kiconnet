8d533b3f6df8d29ed282c3b3c91de3073fab88fc
LongNguyen-51101909 <longnguyenqu@gmail.com>
2016-10-06 16:50:12 +0700

    Initial commit

---
+++ app/src/main/java/freelancer/kiconnet/AbstractActivity.java
@@ -0,0 +1,21 @@
+package freelancer.kiconnet;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
+
+/**
+ * Created by nqlong on 06-Oct-16.
+ */
+public abstract class AbstractActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutId());
+        ButterKnife.bind(this);
+    }
+
+    protected abstract int getLayoutId();
+}
---
+++ app/src/main/java/freelancer/kiconnet/LoginActivity.java
@@ -0,0 +1,19 @@
+package freelancer.kiconnet;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by nqlong on 06-Oct-16.
+ */
+public class LoginActivity extends AbstractActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.login_activity;
+    }
+}
