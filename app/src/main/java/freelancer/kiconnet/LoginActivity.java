24e7676e57213079572f0b5c49fc059a1292e750
LongNguyen-51101909 <longnguyenqu@gmail.com>
2016-10-06 17:03:02 +0700

    commit

--- app/src/main/java/freelancer/kiconnet/LoginActivity.java
+++ app/src/main/java/freelancer/kiconnet/LoginActivity.java
@@ -1,7 +1,10 @@
 package freelancer.kiconnet;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+
+import butterknife.OnClick;
 
 /**
  * Created by nqlong on 06-Oct-16.
@@ -11,7 +14,13 @@ public class LoginActivity extends AbstractActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
-
+    @OnClick(R.id.login_btn)
+    public void returnMain() {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP|
+                Intent.FLAG_ACTIVITY_NO_HISTORY | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
     @Override
     protected int getLayoutId() {
         return R.layout.login_activity;
